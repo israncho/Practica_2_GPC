62166751eb591d4ce1a00ed471652dd0e64ef751
israncho <isragutierrez@ciencias.unam.mx>
2023-09-08 02:19:45 -0600

    de vuelta a como estaba

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
 import cgi
 
-# Encabezado HTTP que indica que se va a generar contenido HTML
-print("Content-type: text/html\n")
+print ("Content-type: text/html")
+print
 
-# HTML generado por el script
-print("<html>")
-print("<head>")
-print('<link rel="stylesheet" href="/css/style.css">')
-print("</head>")
-print("<body>")
-print("<h1>Hola Mundo</h1>")
+print ("""
+Hola Mundo
+""")
 
-# Accede a los datos enviados por el formulario
-form = cgi.FieldStorage()
-user = form["user"].value
-password = form["pass"].value
-
-# Imprime los datos en un formato HTML
-print("<p>User:", user, "</p>")
-print("<p>Pass:", password, "</p>")
-
-print("</body>")
-print("</html>")
+form=cgi.FieldStorage()
+print ("<p>user:", form["user"].value)
+print ("<p>pass:", form["pass"].value)
 
